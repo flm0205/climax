753dbf3cb9a0315caaff98831892dc9eccf0abc7
fralom0205 <francesco.lomuzio@universitybox.com>
2025-10-04 10:25:49 +0200

    Updated LogoSvg.tsx

--- components/LogoSvg.tsx
+++ components/LogoSvg.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Svg, { Text, G } from 'react-native-svg';
+import { Image } from 'react-native';
 
 interface LogoSvgProps {
   width?: number;
@@ -8,24 +8,11 @@ interface LogoSvgProps {
 }
 
 export default function LogoSvg({ width = 400, height = 200, opacity = 1 }: LogoSvgProps) {
-  const scale = width / 400;
-
   return (
-    <Svg width={width} height={height} viewBox="0 0 400 200" style={{ opacity }}>
-      <G transform={`scale(${scale})`}>
-        <Text
-          x="200"
-          y="130"
-          fontFamily="Arial, sans-serif"
-          fontSize="95"
-          fontWeight="900"
-          fill="#D4A239"
-          textAnchor="middle"
-          letterSpacing="8"
-        >
-          CLIMAX
-        </Text>
-      </G>
-    </Svg>
+    <Image
+      source={require('../assets/images/Logo_Transparent.png')}
+      style={{ width, height, opacity }}
+      resizeMode="contain"
+    />
   );
 }
